f2f544859f70bcc2b0b327feb8db8ffddf622158
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2022-12-06 17:07:55 +0000

    Add relevant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #routes to main pages
+  root "apps#index"
+  get "/about", to: "apps#about"
+
+  #route to app pages
+  get "/expense-tracker", to: "apps#expense-tracker"
+  get "/pomodoro-timer", to: "apps#pomodoro-timer"
+  get "/to-do-clone", to: "apps#to-do-clone"
+  get "/calendar-clone", to: "apps#calendar-clone"
 end
