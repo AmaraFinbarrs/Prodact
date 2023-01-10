bd3977b965e49cf12244413c2127702395f7e7e3
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-01-10 20:55:03 +0000

    Create product model, add about action for app, fix route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  
   #routes to main pages
   root "apps#index"
   get"/apps", to: "apps#index" #still root page
   get "/about", to: "apps#about"
 
-  #route to app pages
-  get "/expense-tracker", to: "apps#expense-tracker"
-  get "/pomodoro-timer", to: "apps#pomodoro-timer"
-  get "/to-do-clone", to: "apps#to-do-clone"
-  get "/calendar-clone", to: "apps#calendar-clone"
+  #route to expense-tracker
+  get 'expense_tracker/index'
+  get 'expense_tracker/history'
+
+  # get "/expense-tracker", to: "apps#expense-tracker"
+  # get "/pomodoro-timer", to: "apps#pomodoro-timer"
+  # get "/to-do-clone", to: "apps#to-do-clone"
+  # get "/calendar-clone", to: "apps#calendar-clone"
 end
